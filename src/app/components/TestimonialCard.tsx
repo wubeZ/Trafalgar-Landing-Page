771859ad16c61ee8627fb7e9f34eeb38e2efe4ce
mac <wubezeleke@gmail.com>
2023-09-20 08:11:22 +0300

    fix: Adjusted the scroll of Cards

--- src/app/components/TestimonialCard.tsx
+++ src/app/components/TestimonialCard.tsx
@@ -4,18 +4,20 @@ import picture from "@/assets/images/Group 5.svg";
 
 const TestimonialCard = () => {
   return (
-    <div className="flex flex-col lg:gap-6 items-center bg-gradient-to-r from-sky-500 to-sky-400 text-white p-4 lg:py-20 rounded-2xl lg:px-28">
-      <p className="text-lg lg:text-4xl font-semibold">
+    <div className="flex flex-col lg:gap-6 items-center bg-gradient-to-r from-sky-500 to-sky-400 text-white py-8 lg:py-10 rounded-2xl lg:px-20 w-full">
+      <p className="text-2xl lg:text-3xl font-semibold">
         What our customers are saying
       </p>
-      <p className="border-b-2 border-slate-100 w-14"></p>
-      <div className="flex flex-col px-2 lg:flex-row items-center lg:items-start gap-2 lg:gap-8 mt-4 lg:mt-10 ">
-        <Image src={picture} alt="picture" className="" />
-        <div className="flex flex-col justify-center lg:w-[650px]  text-center lg:text-left">
-          <p className="font-medium lg:text-xl">Edward Newgate</p>
-          <p className="font-light lg:text-lg">Founder Circle</p>
+      <p className="border-b-2 border-slate-100 w-14 mt-4"></p>
+      <div className="flex flex-col px-2 lg:flex-row items-center lg:justify-center lg:items-start gap-2 lg:gap-8 mt-4 lg:mt-8 w-full ">
+        <div className="w-[700px] flex flex-col lg:flex-row gap-4 items-center">
+          <Image src={picture} alt="picture" className="w-fit" />
+          <div className="flex flex-col justify-center lg:py-8  text-center lg:text-left w-40">
+            <p className="font-medium lg:text-xl">Edward Newgate</p>
+            <p className="font-light lg:text-lg">Founder Circle</p>
+          </div>
         </div>
-        <p className="font-light lg:pl-24 text-sm lg:text-xl text-left ">
+        <p className="font-light lg:pl-24 text-sm lg:text-xl text-left mx-8 md:mx-44 lg:mx-0">
           “Our dedicated patient engagement app and web portal allow you to
           access information instantaneously (no tedeous form, long calls, or
           administrative hassle) and securely”
--- src/app/home/TestimonalSection.tsx
+++ src/app/home/TestimonalSection.tsx
@@ -3,8 +3,8 @@ import TestimonialCard from "../components/TestimonialCard";
 
 const TestimonalSection = () => {
   return (
-    <div className="overflow-y-scroll mt-32 lg:mx-8">
-      <div className="flex flex-col lg:flex-row gap-24 lg:gap-40 h-[400px] lg:h-[430px] mb-24 mx-8 lg:mx-32  text-center">
+    <div className="overflow-x-scroll mt-32 lg:mx-8">
+      <div className="flex gap-24 lg:gap-40 h-[430px] mb-24 mx-8 lg:mx-32  text-center">
         {new Array(4)
           .fill(0)
           .map((item, index) => [<TestimonialCard key={index} />])}
